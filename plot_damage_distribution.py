c1c1a2a031c8a1bc19bcbac07ae8c1a5a88aca99
pakreinbold <patrick.ak.reinbold@gmail.com>
2021-10-17 23:34:33 -0400

    Added screenshots

--- plot_damage_distribution.py
+++ plot_damage_distribution.py
@@ -2,8 +2,9 @@
 import matplotlib.pyplot as plt
 from damage_distribution import dmg_distr
 
-dmg, prb = dmg_distr('6d4', 3)
+dmg, prb = dmg_distr('3d10', 0)
 plt.plot(dmg, prb, '-o')
 plt.xlabel('Damage')
 plt.ylabel('Probability')
+plt.xticks(dmg[::4], [int(x) for x in dmg[::4]])
 plt.show()
