2acc8842718e1e620c756e166c7ed1bae6feb1c7
pakreinbold <patrick.ak.reinbold@gmail.com>
2021-10-18 21:03:52 -0400

    Added test for cache saving.

--- tests/test_spell_utility.py
+++ tests/test_spell_utility.py
@@ -1,4 +1,7 @@
-from spell_utility import scrape_spells_fextralife
+import os
+import glob
+import pandas as pd
+from spell_utility import scrape_spells_fextralife, cache_spells
 
 
 def list3(*args, **kwargs):
@@ -14,5 +17,18 @@ def test_scrape_spells_fextralife(monkeypatch):
     monkeypatch.setattr('pandas.read_html', list3)
     try:
         scrape_spells_fextralife()
+        assert False  # fail if no ValueError raised
     except ValueError:
         assert True
+
+
+def test_cache_spells():
+    # Cache some mock DataFrame
+    mock_df = pd.DataFrame()
+    save_name = cache_spells(mock_df, label='mock')
+
+    # Check that the DataFrame was saved to the cache
+    assert save_name in [x.replace('\\', '/') for x in glob.glob('cache/*.csv')]
+
+    # Remove the mock csv
+    os.remove('cache/mock.csv')
