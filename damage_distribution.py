35b74ebbde0c41f6b5adfd2e2cb3d79791030345
pakreinbold <patrick.ak.reinbold@gmail.com>
2021-10-19 21:02:44 -0400

    Added function to calculate the probability distribution of the half damage (when the target passes its saving throw), and a function to combine it with the raw distribution (and modifiers) to get the final adjust probability distribution. Also changed the plotting function to reflect these changes.

--- damage_distribution.py
+++ damage_distribution.py
@@ -6,11 +6,12 @@ Caiado, Camila CS, and Pushpa N. Rathie. "Polynomial coefficients and distributi
     Functions and their Applications, Pala, India, Society for Special Functions and their
     Applications. 2007.
 '''
-from math import floor, comb
+from math import comb, floor
 import numpy as np
 
 
 def poly_coeff(n, k, q):
+
     '''
     Returns the Polynomial Coefficient (PC), defined as the coefficient for x^q resultant from the
     expression
@@ -68,7 +69,7 @@ def distr_sdurv(n, k):
     return y, p
 
 
-def dmg_distr(dice, mod):
+def dmg_distr(dice, mod=0):
     '''
     Returns the damage distribution of a spell/ability assuming that it hits the target.
 
@@ -90,3 +91,66 @@ def dmg_distr(dice, mod):
     dmg += n + mod
 
     return dmg, prb
+
+
+def half_distr(dmg, prb):
+    '''
+    Return the probability distribution of damage when a defender passes a saving throw and is only
+    dealt half-damage. Note that half-points of damage are rounded down.
+
+    Args:
+        dmg (np.array): The options for damage
+        prb (np.array): The probability assigned to each damage option
+
+    Returns:
+        hlf_dmg (np.array): The options of half-damage
+        hlf_prb (np.array): The probability assigned to each half-damage option
+    '''
+    hlf_dmg = np.unique(np.floor(dmg / 2))
+    hlf_prb = np.zeros(hlf_dmg.shape)
+
+    # All dice are even, so the last probability is always mapped to only one value
+    hlf_prb[-1] = prb[-1]
+
+    # The first value has 1 value mapped to it if the damage is odd, 2 values if even
+    n = 2 - (dmg[0] % 2)
+    hlf_prb[0] = prb[:n].sum()
+    hlf_prb[1:-1] = prb[n:-1:2] + prb[n + 1:-1:2]
+
+    return hlf_dmg, hlf_prb
+
+
+def adjusted_distr(dice, mod=0, hit_chance=1, half_damage=False):
+    '''
+    Get the full damage distribution, factoring in hit chance & whether or not the spell will deal
+    half-damage.
+
+    Args:
+        dice (str): Of the format "XdY"
+        mod (int): How much flat damage to add to the dice roll
+        hit_chance (float): Probability of hitting
+        half_damage (bool): Whether or not the target still takes half damage on succeeding a
+            saving throw
+
+    Returns:
+        damage (np.array): The options for damage
+        prob (np.array): The probability for each damage option
+    '''
+
+    raw_dmg, raw_prb = dmg_distr(dice, mod=mod)
+
+    if half_damage:
+        hlf_dmg, hlf_prb = half_distr(raw_dmg, raw_prb)
+
+        # Create new damage options that range from the smallest of the half damage to the largest
+        # of the raw damage
+        damage = np.arange(hlf_dmg[0], raw_dmg[-1] + 1)
+        prob = np.zeros(damage.shape)
+        prob[:len(hlf_prb)] += hlf_prb * (1 - hit_chance)
+        prob[-len(raw_prb):] += raw_prb * hit_chance
+
+    else:
+        damage = np.hstack([0, raw_dmg])
+        prob = np.hstack([1 - hit_chance, hit_chance * raw_prb])
+
+    return damage, prob
